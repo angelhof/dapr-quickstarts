8c47554ec7711206c26706321f36fb0425b1f361
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-10-31 17:10:13 -0400

    modify instructions and notes for an experiment

--- cloudlab-kubernetes-experiment/cloudlab-kubernetes-experiment.sh
+++ cloudlab-kubernetes-experiment/cloudlab-kubernetes-experiment.sh
@@ -32,6 +32,16 @@ dapr init --kubernetes --wait
 ./redis-install-kubernetes.sh
 
 ## Clone the quickstart repo
+https://github.com/angelhof/dapr-quickstarts.git
+cd dapr-quickstarts/tutorials/hello-kubernetes
 
-kubectl apply -f redis-state.yaml
+## Apply the redis installation 
+kubectl apply -f ./deploy/redis.yaml
 
+## See the dashboard by using a new ssh connection just to port forward
+ssh -i ${private_key} -L 9999:127.0.0.1:9999 ${node_username}@${node_address}
+## Very useful to investigate logs etc
+
+## Cleanup by being in the deploy directory and then running the following command
+cd deploy
+kubectl delete -f .
