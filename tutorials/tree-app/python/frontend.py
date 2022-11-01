10fd044b2d2e892f4d0fa139ba1f312e6be3e7cc
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-11-01 16:58:23 -0400

    add some code that inspects open files

--- tutorials/tree-app/python/frontend.py
+++ tutorials/tree-app/python/frontend.py
@@ -1,20 +1,9 @@
-#
-# Copyright 2021 The Dapr Authors
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
 from app import User
 
 import json
 import logging
+import psutil
 from typing import Union
 
 from fastapi import FastAPI
@@ -55,6 +44,11 @@ def book(reservation: Reservation):
         ##       but not for response!
         json_resp = json.loads(resp.data)
         logging.info('Data returned: ' + str(resp.data))
+
+        # proc = psutil.Process()
+        # logging.info(proc.open_files())
+        # for proc in psutil.process_iter():
+            # print(proc.open_files())
     return json_resp
 
 
