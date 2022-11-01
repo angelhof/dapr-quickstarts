089d59cd0b49e883b24901fc73d78ab25323ed52
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-11-01 16:58:36 -0400

    limit concurrency

--- tutorials/tree-app/python/run_frontend.sh
+++ tutorials/tree-app/python/run_frontend.sh
@@ -3,5 +3,9 @@ PORT=5002
 ## If we don't use the --workers option, then it
 ## spawns workers equal to the $WEB_CONCURRENCY variable.
 WORKERS=4
+## We need to tweak this number to avoid any limits being hit
+CONCURRENT_REQUESTS=100
 
-uvicorn frontend:app --port "${PORT}" --workers "${WORKERS}" --reload
+## --reload is only used when debugging
+uvicorn frontend:app --port "${PORT}" --workers "${WORKERS}" --limit-concurrency "${CONCURRENT_REQUESTS}"
+
--- tutorials/tree-app/python/run_server.sh
+++ tutorials/tree-app/python/run_server.sh
@@ -3,5 +3,9 @@ PORT=5001
 ## If we don't use the --workers option, then it
 ## spawns workers equal to the $WEB_CONCURRENCY variable.
 WORKERS=4
+## We need to tweak this number to avoid any limits being hit
+CONCURRENT_REQUESTS=100
 
-uvicorn app:app --port "${PORT}" --workers "${WORKERS}" --reload
+
+## --reload is only used when debugging
+uvicorn app:app --port "${PORT}" --workers "${WORKERS}" --limit-concurrency "${CONCURRENT_REQUESTS}"
